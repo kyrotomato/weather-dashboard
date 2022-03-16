9cd1e79422ffe30f0b802bcca87e7a355f2f261b
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-16 09:47:34 -0500

    added variables to store the info we will need

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
-//create the search form for cities
+//queryselectors
+var button = document.querySelector(".submit");
+var searchForm = document.querySelector(".input");
+const cityName = document.getElementById("city");
+const currentTemp = document.getElementById("tempature");
+const currentHumid = document.getElementById("humidity");
+const currentWind = document.getElementById("wind");
+const uvIndex = document.getElementById("UV");
+const APIkey = "placeholder";
+var fiveForecast = document.getElementById("5Day");
+var todayForecast = document.getElementById("today-weather");
 
-//create a box to display location, and date within a container
-//display the weather for the city I searched
-//5 day forecast, 5 boxes
+
+//function containing the fetch from API
+fetch()
+
