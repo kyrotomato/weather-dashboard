3039bd04f0940149bf64327ea2c7ae342ad10543
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-16 10:49:47 -0500

    fixed some syntax errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,9 @@ const uvIndex = document.getElementById("UV");
 const APIkey = "857149e6cd5b985d6fcc9a8538c6a607";
 var fiveForecast = document.getElementById("5Day");
 var todayForecast = document.getElementById("today-weather");
-var cTemp = "";
-var cHumid = "";
-var cWind = "";
+var cTemp = 0;
+var cHumid = 0;
+var cWind = 0;
 
 button.addEventListener("click", function(response){
 //function containing the fetch from API
@@ -19,7 +19,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=" +searchForm.value+ "&
     .then(Response => Response.json())
     .then(data => {
     //store data in variables
-    //var city = response.data.name[""];
+    var city = response.data["name"];
     cTemp = data["main"]["temp"];
     cHumid = data["main"]["humidity"];
     cWind = data["wind"]["speed"];
