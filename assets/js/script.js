f184a17da05172cc85f3a237e3ec4e60da3eb04a
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-16 10:41:01 -0500

    added api and response to store api data in variable for display on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,26 @@ const currentTemp = document.getElementById("tempature");
 const currentHumid = document.getElementById("humidity");
 const currentWind = document.getElementById("wind");
 const uvIndex = document.getElementById("UV");
-const APIkey = "placeholder";
+const APIkey = "857149e6cd5b985d6fcc9a8538c6a607";
 var fiveForecast = document.getElementById("5Day");
 var todayForecast = document.getElementById("today-weather");
+var cTemp = "";
+var cHumid = "";
+var cWind = "";
 
-
+button.addEventListener("click", function(response){
 //function containing the fetch from API
-fetch()
-
+fetch("https://api.openweathermap.org/data/2.5/weather?q=" +searchForm.value+ "&appid=" + APIkey)
+    .then(Response => Response.json())
+    .then(data => {
+    //store data in variables
+    //var city = response.data.name[""];
+    cTemp = data["main"]["temp"];
+    cHumid = data["main"]["humidity"];
+    cWind = data["wind"]["speed"];
+    cityName.innerText = city;
+    currentTemp.innerText = cTemp;
+    currentHumid.innerText = cHumid;
+    currentWind.innerText = cWind;
+    })
+})
